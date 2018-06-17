f5e8ca54e66bacb5b2d241dda5e6a77ce9e62a7d
Ismail Kheiry <ikheiry@sqli.com>
2018-06-17 17:47:36 +0200

    DialogApp, custom dialog communication with host

--- app/src/main/java/com/ikheiry/dialogapp/MainActivity.java
+++ app/src/main/java/com/ikheiry/dialogapp/MainActivity.java
@@ -3,19 +3,31 @@ package com.ikheiry.dialogapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.ikheiry.dialogapp.dialog.MyCustomDialog;
 import com.ikheiry.dialogapp.dialog.MyThirdDialog;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyCustomDialog.OnLoginListener {
+
+    private TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.dialog_response);
     }
 
     public void openDialog(View view) {
-        new MyThirdDialog().show(getSupportFragmentManager(), "mythirddialog");
+        MyCustomDialog customDialog = new MyCustomDialog();
+        customDialog.show(getSupportFragmentManager(), "custom_dialog");
 
+    }
+
+    @Override
+    public void onLogin(String message) {
+        display.setText(message);
     }
 }
--- app/src/main/java/com/ikheiry/dialogapp/dialog/MyCustomDialog.java
+++ app/src/main/java/com/ikheiry/dialogapp/dialog/MyCustomDialog.java
@@ -1,22 +1,77 @@
 package com.ikheiry.dialogapp.dialog;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.ikheiry.dialogapp.R;
+
 public class MyCustomDialog extends DialogFragment {
+
+    private EditText txtUsername, txtPassword;
+    private OnLoginListener loginListener;
+
+    public interface OnLoginListener {
+        public void onLogin(String message);
+    }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Fire Missile?");
-        
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.custom_dialog_layout, null);
+        builder.setView(view);
+
+        txtUsername = view.findViewById(R.id.user_name);
+        txtPassword = view.findViewById(R.id.user_password);
+
+        builder.setPositiveButton("Sign In", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String username = txtUsername.getText().toString();
+                String password = txtPassword.getText().toString();
+                if (!username.isEmpty() && !password.isEmpty()) {
+                    if (username.equals(getActivity().getResources().getString(R.string.username)) &&
+                            password.equals(getActivity().getResources().getString(R.string.password))) {
+                        loginListener.onLogin("Welcome " + getActivity().getResources().getString(R.string.user_real_name));
+                    } else {
+                        loginListener.onLogin("Sorry, login failed!");
+                    }
+                } else
+                    loginListener.onLogin("Username or Password cannot be empty!");
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getActivity(), "Operation cancled!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Activity activity = (Activity) context;
+        try {
+            loginListener = (OnLoginListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnLoginListener methods");
+        }
     }
 }
